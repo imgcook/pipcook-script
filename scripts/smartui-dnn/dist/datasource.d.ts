00f01b79b5776e941bb702565d273b62ffd88b2b
queyue.crk <queyue.crk@alibaba-inc.com>
2021-07-20 19:19:31 +0800

    remove dist

--- scripts/smartui-dnn/dist/datasource.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @file This plugin is to access classification image data from different sources. Make sure that
- * the data is conform to expectation.
- */
-import { DatasourceEntry } from '@pipcook/core';
-import * as Datacook from '@pipcook/datacook';
-declare const OdpsDataCollect: DatasourceEntry<Datacook.Dataset.Types.Sample, Datacook.Dataset.Types.TableDatasetMeta>;
-export default OdpsDataCollect;
-//# sourceMappingURL=datasource.d.ts.map
--- scripts/smartui-dnn/dist/model.d.ts
+++
@@ -1,5 +0,0 @@
-import { ModelEntry } from '@pipcook/core';
-import type { Dataset } from '@pipcook/datacook';
-declare const main: ModelEntry<Dataset.Types.Sample, Dataset.Types.TableDatasetMeta>;
-export default main;
-//# sourceMappingURL=model.d.ts.map
