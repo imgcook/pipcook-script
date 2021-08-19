8bc75a6fa36e1e721560f4d097c831446536f9f6
数斯 <wangxiaojian.wxj@alibaba-inc.com>
2021-08-19 10:52:18 +0800

    js script

--- scripts/text-classification-bayes/src/js/model.js
+++ scripts/text-classification-bayes/src/js/model.js
@@ -8,9 +8,8 @@ const { MultinomialNB } = DataCook.Model.NaiveBayes;
 const { CountVectorizer } = DataCook.Text;
 const { accuracyScore  } = DataCook.Metrics;
 
-const textProcessing = (row_data, row_class, mode='cn') => {
+const textProcessing = (row_data, mode='cn') => {
     let words_list = [];
-    let class_list = [];
     load();
     row_data.forEach((data, i) => {
       if (mode == 'cn'){
@@ -19,46 +18,36 @@ const textProcessing = (row_data, row_class, mode='cn') => {
       } else {
         words_list.push(data)
       }
-      class_list.push(row_class[i]);
     });
-    return [ words_list, class_list ]
+    return words_list
 }
 
-const modelDefine = async (options, context) => {
-  const { recoverPath } = options;
-  let classifier = new MultinomialNB();
-  let vectorizer = new CountVectorizer();
 
-  /*const modelJson = await fs.readFile(path.join(recoverPath, 'model.json'));
-  const vectorizerJson =  await fs.readFile(path.join(recoverPath, 'vectorizer.json'));
-  if(modelJson) classifier.load(modelJson);
-  if(vecorizerJson) vectorizer.load(vectorizerJson);*/
-  return [ classifier, vectorizer ];
-};
-
-const modelTrain = async (runtime, options, context, model) => {
+const train = async (runtime, options, context) => {
   const { mode = 'cn' } = options;
   const { modelDir } = context.workspace;
-  const [ classifier, vectorizer ] = model;  
+  const classifier = new MultinomialNB();
+  const vectorizer = new CountVectorizer();
 
-  const rawData = [];
-  const rawClass = [];
+  const trainData = [];
+  const trainClass = [];
   // access train samples
   const trainSamples = await runtime.dataset.train.nextBatch(-1);
   trainSamples.forEach((d) => {
-    rawData.push(d.data);
-    rawClass.push(d.label.toString());
+    trainData.push(d.data);
+    trainClass.push(d.label.toString());
   });
   // process text dataset
-  const [ trainWordsList, trainClassList ] = textProcessing(rawData, rawClass);
+  const trainWordsList = textProcessing(rawData);
   let stopWords = mode === 'en' ? en : cn;
   vectorizer.initDict(trainWordsList, stopWords);
   const trainVector = vectorizer.transform(trainWordsList);
   // train model
-  await classifier.train(trainVector, trainClassList);
+  await classifier.train(trainVector, trainClass);
   // predict
   const predClass = classifier.predict(trainVector);
-  const acc = accuracyScore(rawClass, predClass);
+
+  const acc = accuracyScore(trainClass, predClass);
   console.log(`train accuracy: ${acc}`);
   // save model file 
   await fs.writeFile(path.join(modelDir, 'model.json'), classifier.toJson());
@@ -66,18 +55,47 @@ const modelTrain = async (runtime, options, context, model) => {
   await runtime.saveModel(modelDir);
 };
 
-let predictModel;
+const modelLoad = async (context) => {
+  const { modelDir } = context.workspace;
+  const classifier = new MultinomialNB();
+  const vectorizer = new CountVectorizer();
+
+  const modelFs = await fs.readFile(path.join(modelDir, 'model.json'));
+  
+  const modelJson = modelFs.toString();
+  const vectorizerFs =  await fs.readFile(path.join(modelDir, 'vectorizer.json'));
+  const vectorizerJson = vectorizerFs.toString();
+  //console.log(modelJson)
+  //console.log(vectorizerJson)
+  if (modelJson) classifier.load(modelJson);
+  if (vectorizerJson) vectorizer.load(vectorizerJson);
+  return [ classifier, vectorizer ];
+};
+
+const predict = async (runtime, options, context) => {
+  const [ classifier, vectorizer ] = await modelLoad(context);
+  // access predict samples
+  const predictData = [];
+  const predictSamples = await runtime.dataset.predicted.nextBatch(-1);
+  predictSamples.forEach((d) => {
+    predictData.push(d.data);
+  });
+  // process text dataset
+  const predictWordsList = textProcessing(predictData);
+  const predictVector = vectorizer.transform(predictWordsList);
+  const predClasses = classifier.predict(predictVector).dataSync();
+  const predIds = predClasses.map((predClass) => classifier.classMap[predClass])
+  const predProbs = classifier.predictProba(predictVector).arraySync();
+  const predictResult = predClasses.map((predClass, i)=>{
+    const res = {id: predIds[i], category: predClass, score: predProbs[i][predIds[i]]}
+    return res
+  })
+  console.log(predictResult);
+  return predictResult
+}
 
 module.exports = {
-  train: async (runtime, options, context) => {
-    let model = await modelDefine(options, context);
-    await modelTrain(runtime, options, context, model);
-  },
-  // predict: async (runtime, options, context) => {
-  //   if (!predictModel) {
-  //     predictModel = await modelLoad(options, context);
-  //     await 
-  //   }
-  // }
+  train,
+  predict
 }
 
--- scripts/text-classification-bayes/webpack.config.js
+++ scripts/text-classification-bayes/webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
   target: "node",
   entry: {
-    model: ["./src/model.ts"],
     datasource: ["./src/datasource.ts"]
   },
   module: {
@@ -24,13 +23,7 @@ module.exports = {
     libraryTarget: 'umd'
   },
   externals: {
-<<<<<<< HEAD
-    '@pipocook/datacook': 'commonjs2 @pipcook/datacook'
-=======
-    '@pipcook/boa': 'commonjs2 @pipcook/boa',
-    '@pipcook/core': 'commonjs2 @pipcook/core',
-    '@pipcook/datacook': 'commonjs2 @pipcook/datacook'
->>>>>>> fe00a8e14b66b37e1895b56297fe223295f18bda
+    '@pipcook/boa': 'commonjs2 @pipcook/boa'
   },
   mode: 'development'
 };
